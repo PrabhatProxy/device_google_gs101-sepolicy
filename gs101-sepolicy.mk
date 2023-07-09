4db00623e6a08b9824a46a3f2890eccd349a79f9
PrabhatProxy <prabhat@e.email>
2023-07-09 12:11:12 +0530

    import sepolicies from evolutionx

--- gs101-sepolicy.mk
+++ gs101-sepolicy.mk
@@ -1,24 +1,36 @@
-# sepolicy that are shared among devices using whitechapel
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/whitechapel/vendor/google
-
-# unresolved SELinux error log with bug tracking
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/tracking_denials
-
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs101-sepolicy/private
-
 # ConnectivityThermalPowerManager
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/connectivity_thermal_power_manager
 
+# ramdump
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/ramdump
+
+# turbo adapter
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/turbo_adapter
+
+# twoshay
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/input
+
+# google_battery service
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
+
+# sepolicy that are shared among devices using whitechapel
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/whitechapel/vendor/google
+
+# unresolved SELinux error log with bug tracking
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/tracking_denials
+
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs101/sepolicy/private
+
 # Display
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/display/common
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/display/gs101
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/display/common
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/display/gs101
 
 # Micro sensor framework (usf)
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/usf
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/usf
 
 # system_ext
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/gs101-sepolicy/system_ext/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101-sepolicy/system_ext/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/gs101/sepolicy/system_ext/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101/sepolicy/system_ext/private
 
 #
 # Pixel-wide
@@ -33,12 +45,12 @@ BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/sscoredump
 
 # Public
-PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs101-sepolicy/public
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs101/sepolicy/public
 
 # pKVM
 ifeq ($(TARGET_PKVM_ENABLED),true)
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/pkvm
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/pkvm
 endif
 
 # Health HAL
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/health
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/health
